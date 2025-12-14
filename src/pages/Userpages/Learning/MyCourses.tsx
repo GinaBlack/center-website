e4ad50ee6129cf5961a556d8fd982e38fabfb6e3
GinaBlack <168082277+GinaBlack@users.noreply.github.com>
2025-12-14 20:19:07 +0100

    Update MyCourses.tsx

--- src/pages/Userpages/Learning/MyCourses.tsx
+++ src/pages/Userpages/Learning/MyCourses.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const  MyCourses = () => {
-    return (
-        <div>
-            <h1>My Courses</h1>
-        </div>
-    );
+const MyCourses = () => {
+  return (
+    <div>
+      <h1>My Courses</h1>
+      <p>This is the My Courses page.</p>
+      {/* Add your courses content here */}
+    </div>
+  );
 };
 
 export default MyCourses;
